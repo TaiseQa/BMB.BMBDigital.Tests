60b2a67caba33f19a8373d9a846a94ffa6b6f0f7
Bruno Souza <bruno.souza@inmetrics.com.br>
2022-06-29 12:50:05 -0300

    subindo ajuste para lidar com geolocalização 02

--- web-cucumber-allure/src/main/java/framework/utils/Utils.java
+++ web-cucumber-allure/src/main/java/framework/utils/Utils.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Utils {
@@ -21,11 +23,19 @@ public class Utils {
         if (driver == null) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             ChromeOptions options = new ChromeOptions();
+            Map<String, Object> prefs = new HashMap<>();
+            Map<String, Object> profile = new HashMap<>();
+            Map<String, Object> contentSettings = new HashMap<>();
+            contentSettings.put("geolocation", 1);
+            profile.put("managed_default_content_settings", contentSettings);
+            prefs.put("profile", profile);
+            options.setExperimentalOption("prefs", prefs);
             options.addArguments("--headless");
             options.addArguments("--disable-gpu");
             options.addArguments("--disable-geolocation");
             options.addArguments("--window-size=1400,800");
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+            options.setCapability(ChromeOptions.CAPABILITY, options);
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
             wait = new WebDriverWait(driver, 60);
