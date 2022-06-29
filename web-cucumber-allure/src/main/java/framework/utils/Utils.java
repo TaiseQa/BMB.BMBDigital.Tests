1ca37eea36e392a5a27ca2c4fd65cda82735b0fe
Bruno Souza <bruno.souza@inmetrics.com.br>
2022-06-29 15:06:37 -0300

    subindo atualização de ajuste sobre solicitação de geolocalização

--- web-cucumber-allure/src/main/java/framework/utils/Utils.java
+++ web-cucumber-allure/src/main/java/framework/utils/Utils.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.HashMap;
@@ -21,21 +22,15 @@ public class Utils {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             ChromeOptions options = new ChromeOptions();
             Map<String, Object> prefs = new HashMap<>();
-            Map<String, Object> profile = new HashMap<>();
-            Map<String, Object> contentSettings = new HashMap<>();
-            contentSettings.put("geolocation", 1);
-            profile.put("managed_default_content_settings", contentSettings);
-            prefs.put("profile", profile);
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            prefs.put("profile.default_content_setting_values.geolocation", 2);
             options.setExperimentalOption("prefs", prefs);
             options.addArguments("--headless");
             options.addArguments("--disable-gpu");
-            options.addArguments("--disable-geolocation");
             options.addArguments("--window-size=1400,800");
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-            options.setCapability(ChromeOptions.CAPABILITY, options);
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
             wait = new WebDriverWait(driver, 60);
