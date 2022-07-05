107ca6acd969c0ad28906f5853fecb39c1d9d307
Bruno Souza <bruno.souza@inmetrics.com.br>
2022-07-05 18:50:51 -0300

    adicionando dependencia do lombok para usar anotação que ignora exceções e adicionando no metodo getDriver o codigo que manda logs que o chrome cospe para um arquivo txt no targht, assim evitando erros no console do TFS

--- web-cucumber-allure/src/main/java/framework/utils/Utils.java
+++ web-cucumber-allure/src/main/java/framework/utils/Utils.java
@@ -1,14 +1,19 @@
 package framework.utils;
 
+import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.service.DriverService;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
 
 public class Utils {
 
@@ -19,19 +24,23 @@ public class Utils {
 
     }
 
+    @SneakyThrows
     public static WebDriver getDriver() {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
             Map<String, Object> prefs = new HashMap<>();
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-            System.setProperty("webdriver.chrome.silentOutput", "true");
+            DriverService.Builder<ChromeDriverService, ChromeDriverService.Builder> serviceBuilder = new ChromeDriverService.Builder();
+            ChromeDriverService chromeDriverSerive = serviceBuilder.build();
+            chromeDriverSerive.sendOutputTo(new FileOutputStream("target/chromedriver_log.txt", true));
+
             prefs.put("profile.default_content_setting_values.geolocation", 2);
             options.setExperimentalOption("prefs", prefs);
             options.addArguments("--headless");
             options.addArguments("--disable-gpu");
             options.addArguments("--window-size=1400,800");
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-            driver = new ChromeDriver(options);
+            driver = new ChromeDriver(chromeDriverSerive, options);
             driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
             wait = new WebDriverWait(driver, 60);
 
