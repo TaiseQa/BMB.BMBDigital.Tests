07e84c6580ccc081f59ac2cabca639c9b95973a2
Bruno Souza <bruno.souza@inmetrics.com.br>
2022-07-05 14:18:20 -0300

    adicionando script para corrigir log error02

--- web-cucumber-allure/src/main/java/framework/utils/Utils.java
+++ web-cucumber-allure/src/main/java/framework/utils/Utils.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.HashMap;
@@ -25,10 +24,10 @@ public class Utils {
             ChromeOptions options = new ChromeOptions();
             Map<String, Object> prefs = new HashMap<>();
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            System.setProperty("webdriver.chrome.silentOutput", "true");
             prefs.put("profile.default_content_setting_values.geolocation", 2);
             options.setExperimentalOption("prefs", prefs);
             options.addArguments("--headless");
-            options.addArguments("--log-level=3");
             options.addArguments("--disable-gpu");
             options.addArguments("--window-size=1400,800");
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
